b6115e453b13787ee27f9ab0921b0e36e5237f4d
RekGRpth <rekgrpth@gmail.com>
2023-05-10 09:54:51 +0500

    user

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-docker build --progress=plain --tag "ghcr.io/rekgrpth/gpdb.docker:${INPUTS_BRANCH:-centos}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-centos.Dockerfile}" . 2>&1 | tee build.log
+docker build --tag "ghcr.io/rekgrpth/gpdb.docker:${INPUTS_BRANCH:-centos}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-centos.Dockerfile}" . 2>&1 | tee build.log
