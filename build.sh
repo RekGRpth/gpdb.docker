b6d6ef0de706a1d59db06e2575742947eaf7e4fb
RekGRpth <rekgrpth@gmail.com>
2024-03-02 16:38:13 +0500

    up

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -eux
-
-docker build --tag "ghcr.io/rekgrpth/gpdb.docker:${INPUTS_BRANCH:-centos}" $(env | grep -E '^DOCKER_' | grep -v ' ' | sort -u | sed 's@^@--build-arg @g' | paste -s -d ' ') --file "${INPUTS_DOCKERFILE:-centos.Dockerfile}" . 2>&1 | tee build.log
