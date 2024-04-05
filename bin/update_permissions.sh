a5ffb05962f14ab81c312f59155f789373b79c14
RekGRpth <rekgrpth@gmail.com>
2024-04-05 16:54:34 +0500

    up

--- bin/update_permissions.sh
+++ bin/update_permissions.sh
@@ -3,8 +3,8 @@
 exec 2>&1
 set -ex
 if [ -n "$GROUP" ] && [ -n "$GROUP_ID" ]; then
-    find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+    find "$HOME" ! -group "$GROUP" -exec sudo chgrp "$GROUP_ID" {} \;
 fi
 if [ -n "$USER" ] && [ -n "$USER_ID" ]; then
-    find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
+    find "$HOME" ! -user "$USER" -exec sudo chown "$USER_ID" {} \;
 fi
