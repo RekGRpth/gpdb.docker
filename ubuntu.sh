4e3495ae3bf5db2053e2044565ff4b52fc59e39a
RekGRpth <rekgrpth@gmail.com>
2024-12-02 10:16:37 +0500

    ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eux
+
+docker network create --attachable --ipv6 --subnet 2001:db8::/112 --opt com.docker.network.bridge.name=docker docker || echo $?
+docker volume create gpdb
+docker stop ubuntu || echo $?
+docker rm ubuntu || echo $?
+docker run \
+    --detach \
+    --hostname ubuntu \
+    --init \
+    --mount type=volume,source=gpdb,destination=/home/gpadmin \
+    --name ubuntu \
+    --network name=docker \
+    --privileged \
+    --pull always \
+    --restart always \
+    hub.adsw.io/library/docker:20.10.17-x64 sleep infinity
