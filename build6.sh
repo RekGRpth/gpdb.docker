6233c2478fea00be1968d2037a0caab181a71415
Georgy Shelkovy <rekgrpth@gmail.com>
2025-11-08 08:56:33 +0500

    optimize

--- build6.sh
+++ build6.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -eux
 
+export GP_MAJOR=6
 docker build \
-    --build-arg GP_MAJOR=6 \
-    --file gpdb6.Dockerfile \
+    --file "gpdb$GP_MAJOR.Dockerfile" \
     --pull \
     --network=host \
-    --tag gpdb6 \
-    . 2>&1 | tee build6.log
+    --tag "gpdb$GP_MAJOR" \
+    . 2>&1 | tee "build$GP_MAJOR.log"
